db639587eae46913da1e8bc8862b202b269f7ec6
Ellen Spertus <ellen.spertus@gmail.com>
2013-11-17 10:46:23 -0800

    Fixed core Blockly bug (?) with flyout.

--- core/flyout.js
+++ core/flyout.js
@@ -189,6 +189,9 @@ Blockly.Flyout.prototype.getMetrics_ = function() {
  */
 Blockly.Flyout.prototype.setMetrics_ = function(yRatio) {
   var metrics = this.getMetrics_();
+  if (!metrics) {
+    return;
+  }
   if (goog.isNumber(yRatio.y)) {
     this.workspace_.scrollY =
         -metrics.contentHeight * yRatio.y - metrics.contentTop;
