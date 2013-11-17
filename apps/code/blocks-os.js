173c0e60a980e9a3cb3fdd70be78cd531b902322
Ellen Spertus <ellen.spertus@gmail.com>
2013-11-17 11:18:42 -0800

    Switched blocks-os.js to using simpler block declaration framework.

--- apps/code/blocks-os.js
+++ apps/code/blocks-os.js
@@ -1,49 +1,53 @@
-var BASE_OS_HELP_URL_ = BlocklyLua.BASE_HELP_URL + 'Os.';
-OS_BLOCK_COLOUR_ = 30;
+/**
+ * Blockly Lua: ComputerCraft File System API
+ *
+ * Copyright 2013 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Blockly.Blocks['os_terminate'] = {
-  // Terminate program with error message.
-  init: function() {
-    this.setColour(OS_BLOCK_COLOUR_);
-    this.appendValueInput('MSG')
-        .appendTitle('terminate with error ');
-    this.setTooltip('End the program with the provided error message.  ' +
-        'This should not be used for normal termination.');
-    // This has a previous, but not a following, statement.
-    this.setPreviousStatement(true);
+/**
+ * @fileoverview Blocks for ComputerCraft File System (FS) API.
+ * @author ellen.spertus@gmail.com (Ellen Spertus)
+ */
+'use strict';
+
+BlocklyLua.OS_BLOCK_COLOUR_ = 30;
+
+BlocklyLua.OS_FUNCS_ = [
+  {name: 'terminate',
+   // This has a previous, but not a following, statement, which is rare.
+   stmtConns: BlocklyLua.StmtConns.PREVIOUS,
+   text: 'terminate with error %1',
+   args: [['MSG', null]],
+   tooltip:
+   'End the program with the provided error message.  ' +
+   'This should not be used for normal termination.'
+  },
+  {name: 'sleep',
+   stmtConns: BlocklyLua.StmtConns.PREVIOUS | BlocklyLua.StmtConns.NEXT,
+   text: 'sleep %1 seconds',
+   args: [['VALUE', 'Number']],
+   tooltip: 'Sleep for the specified number of seconds.'
   }
-};
+];
 
-Blockly.Lua['os_terminate'] = function(block) {
-  // Generate Lua for terminating program with error message.
-  var message = Blockly.Lua.valueToCode(block, 'MSG',
-      Blockly.Lua.ORDER_NONE) || '"error"';
-  return 'error(' + message + ')\n';
-};
+for (var i = 0; i < BlocklyLua.OS_FUNCS_.length; i++) {
+  BlocklyLua.buildValueBlock(
+    'os', BlocklyLua.OS_BLOCK_COLOUR_, BlocklyLua.OS_FUNCS_[i]);
+}
 
 // Added in order to continue to support programs with the old block name.
 Blockly.Blocks['terminate'] = Blockly.Blocks['os_terminate'];
 Blockly.Lua['terminate'] = Blockly.Lua['os_terminate'];
-
-Blockly.Blocks['os_sleep'] = {
-  // Sleep for the specified number of seconds.
-  init: function() {
-    this.setColour(OS_BLOCK_COLOUR_);
-    this.appendDummyInput()
-        .appendTitle('sleep');
-    this.appendValueInput('VALUE')
-        .setCheck('Number')
-        .appendTitle('seconds');
-    this.setTooltip('Sleep for the specified number of seconds.')
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setHelpUrl(BASE_OS_HELP_URL_ + 'sleep');
-  }
-};
-
-Blockly.Lua['os_sleep'] = function(block) {
-  // Generate Lua for sleeping the specified number of seconds.
-  var argument0 = Blockly.Lua.valueToCode(block, 'VALUE',
-      Blockly.Lua.ORDER_NONE) || '1';
-  return 'sleep(' + argument0 + ')\n';
-};
