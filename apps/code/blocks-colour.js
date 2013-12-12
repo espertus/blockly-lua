f72c89e2c3c3d9756b8a708ae91fd173bdae51f0
Ellen Spertus <ellen.spertus@gmail.com>
2013-12-12 15:12:56 -0800

    Rewrote colour palette names (e.g., colour.pink) instead of numbers. Added colour selector by name. Added remaining blocks from colour API. Added VarArgs.generateLua_ to put var args at the end of the parameter list.

--- apps/code/blocks-colour.js
+++ apps/code/blocks-colour.js
@@ -21,46 +21,33 @@
  * @fileoverview Colour blocks for ComputerCraft.
  * @author ellen.spertus@gmail.com (Ellen Spertus)
  */
+'use strict';
+
+Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_ = 80;
 
 /**
  * An array of colours for the palette.  The rgb attributes may be tweaked,
  * but the value attributes must not, since they correspond to ones on
  * "http://computercraft.info/wiki/Colors_%28API%29".
  */
-var COLOURS_ = [
-  // WHITE
-  { rgb: '#f0f0f0', value: 1 << 0 },
-  // ORANGE
-  { rgb: '#f2b233', value: 1 << 1 },
-  // MAGENTA
-  { rgb: '#e57fd8', value: 1 << 2 },
-  // LIGHT_BLUE
-  { rgb: '#99b2f2', value: 1 << 3 },
-  // YELLOW
-  { rgb: '#dede6c', value: 1 << 4 },
-  // LIME
-  { rgb: '#7fcc19', value: 1 << 5 },
-  // PINK
-  { rgb: '#f2b2cc', value: 1 << 6 },
-  // GREY
-  { rgb: '#4c4c4c', value: 1 << 7 },
-  // LIGHT_GREY
-  { rgb: '#999999', value: 1 << 8 },
-  // CYAN
-  { rgb: '#4c99b2', value: 1 << 9 },
-  // PURPLE
-  { rgb: '#b266e5', value: 1 << 10 },
-  // BLUE
-  { rgb: '#253192', value: 1 << 11 },
-  // BROWN
-  { rgb: '#7f664c', value: 1 << 12 },
-  // GREEN
-  { rgb: '#57a64e', value: 1 << 13 },
-  // RED
-  { rgb: '#cc4c4c', value: 1 << 14 },
-  // BLACK
-  { rgb: '#191919', value: 1 << 15 }
-];
+var COLOURS_ = {
+  white: {rgb: '#f0f0f0', value: 1 << 0 },
+  orange: {rgb: '#f2b233', value: 1 << 1 },
+  magenta: {rgb: '#e57fd8', value: 1 << 2 },
+  lightBlue: {rgb: '#99b2f2', value: 1 << 3 },
+  yellow: {rgb: '#dede6c', value: 1 << 4 },
+  lime: {rgb: '#7fcc19', value: 1 << 5 },
+  pink: {rgb: '#f2b2cc', value: 1 << 6 },
+  grey: {rgb: '#4c4c4c', value: 1 << 7 },
+  lightGrey: {rgb: '#999999', value: 1 << 8 },
+  cyan: {rgb: '#4c99b2', value: 1 << 9 },
+  purple: {rgb: '#b266e5', value: 1 << 10 },
+  blue: {rgb: '#253192', value: 1 << 11 },
+  brown: {rgb: '#7f664c', value: 1 << 12 },
+  green: {rgb: '#57a64e', value: 1 << 13 },
+  red: {rgb: '#cc4c4c', value: 1 << 14 },
+  black: {rgb: '#191919', value: 1 << 15 }
+};
 
 /**
  * The order in which colours should appear on the palette.
@@ -68,7 +55,10 @@ var COLOURS_ = [
  * This overrides a value defined in core/field_colour.js
  * (via blockly_[un]compressed.js).
  */
-Blockly.FieldColour.COLOURS = COLOURS_.map(function(c) {return c.rgb;});
+Blockly.FieldColour.COLOURS = Object.keys(COLOURS_).map(
+  function(name) {
+    return COLOURS_[name].rgb;
+  });
 
 /**
  * Number of columns in the palette.
@@ -81,10 +71,10 @@ Blockly.Blocks['colour_picker'] = {
   // Colour picker.
   init: function() {
     this.setHelpUrl('http://computercraft.info/wiki/Colors_%28API%29');
-    this.setColour(20);
+    this.setColour(Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_);
     this.appendDummyInput()
         .appendTitle(new Blockly.FieldColour('#cc4c4c'), 'COLOUR');
-    this.setOutput(true, 'Colour');
+    this.setOutput(true, 'Number');
     this.setTooltip(
       'Choose among colours (from left to right, top to bottom):\n' +
       'white, orange, magenta, light blue,\n' +
@@ -94,15 +84,79 @@ Blockly.Blocks['colour_picker'] = {
   }
 };
 
+// TODO: Change block colour to match chosen colour, if not too hard.
+Blockly.ComputerCraft.buildValueBlock(
+  'colour',
+  Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_,
+  {blockName: 'list',
+   text: '%1',
+   args: [['COLOUR',
+           new Blockly.FieldDropdown(Object.keys(COLOURS_).map(
+             function(name) {
+               return [Blockly.ComputerCraft.convertFromCamelCase(name).
+                       replace('_', ' '),
+                       name];
+             }))]],
+   output: 'Number',
+   tooltip: 'Choose among colours by name.'});
+
+Blockly.Lua['colour_list'] = function(block) {
+  var code = 'colour.' + block.getTitleValue('COLOUR');
+  return [code, Blockly.Lua.ORDER_HIGH];
+};
+
 Blockly.Lua['colour_picker'] = function(block) {
   // Generate Lua code for the selected ComputerCraft colour.
   var colour = block.inputList[0].titleRow[0].colour_;
-  for (var i = 0; i < COLOURS_.length; i++) {
-    var entry = COLOURS_[i];
+  var keys = Object.keys(COLOURS_);
+  for (var x = 0; x < keys.length; x++) {
+    var key = keys[x];
+    var entry = COLOURS_[key];
     if (entry.rgb == colour) {
-      return [entry.value, Blockly.Lua.ORDER_HIGH];
+      return ['colour.' + key, Blockly.Lua.ORDER_HIGH];
     }
   }
-  // This line shouldn't be reached.
-  window.alert('Error from colour_picker.');
+  goog.asserts.fail('Error in colour_picker');
 };
+
+Blockly.ComputerCraft.buildVarArgsBlock(
+  'colour',
+  Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_,
+  {funcName: 'combine',
+   text: 'combine colours %v into set',
+   varArgName: 'colour',
+   varArgType: 'Number',
+   varArgCount: 2,
+   varContainerName: 'colours',
+   output: 'Number',
+   tooltip:
+   'Combine one or more colours (or sets of colours) into a set.\n' +
+   'To change the number of colours, click on the star.'});
+
+Blockly.ComputerCraft.buildVarArgsBlock(
+  'colour',
+  Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_,
+  {funcName: 'subtract',
+   text: 'remove colours %v from set %1',
+   args: [['Set', 'Number']],
+   varArgName: 'colour',
+   varArgType: 'Number',
+   varArgCount: 1,
+   varContainerName: 'colours to remove',
+   output: 'Number',
+   tooltip:
+   'Remove one or more colours from the specified set of colours.\n' +
+   'To change the number of colours to remove, click on the star.\n' +
+   'A set can be built with the "combine" block.'});
+
+Blockly.ComputerCraft.buildValueBlock(
+  'colour',
+  Blockly.ComputerCraft.COLOUR_BLOCK_COLOUR_,
+  {funcName: 'test',
+   text: 'does set %1 contain colour %2?',
+   args: [['Set', 'Number'],
+          ['Colour', 'Number']],
+   output: 'Boolean',
+   tooltip:
+   'Test whether the given colour is in the set.\n' +
+   'A set can be built with the "combine" block.'});
